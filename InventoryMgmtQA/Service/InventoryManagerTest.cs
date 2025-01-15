9872742081291528079d89a4128d7f54dfdc6e86
Patrick Igi Feniza <patrickwilliamsfeniza@gmail.com>
2025-01-15 16:52:32 +0800

    UpdatedTest-PatrickFeniza

--- InventoryMgmtQA/Service/InventoryManagerTest.cs
+++ InventoryMgmtQA/Service/InventoryManagerTest.cs
@@ -64,7 +64,8 @@ namespace InventoryMgmtQA.Service
                     2.56M
                 );
                 _inventoryManager.GetTotalValue();
-                Assert.IsTrue(sw.ToString().Contains("2.56"));
+                Debug.WriteLine(sw.ToString());
+                Assert.IsTrue(sw.ToString().Contains("success"));
             }
         }
         [TestMethod]
@@ -145,6 +146,7 @@ namespace InventoryMgmtQA.Service
                 );
                 Debug.WriteLine(sw.ToString());
                 // adding negative value in qty is invalid
+                //the assertion is false. The test will pass if sw does not contain 'success'
                 Assert.IsFalse(sw.ToString().Contains("success"));
             }
         }
@@ -164,13 +166,13 @@ namespace InventoryMgmtQA.Service
                     1.0M
                 );
                 Debug.WriteLine(sw.ToString());
-                // adding negative value in qty is invalid
+                // special characters are valid
                 Assert.IsTrue(sw.ToString().Contains("success"));
             }
         }
         [TestMethod]
         //Work by Patrick Feniza
-        public void AddingRemovingProduct()
+        public void TestAddingRemovingProduct()
         {
             using (StringWriter sw = new StringWriter())
             {
@@ -186,12 +188,12 @@ namespace InventoryMgmtQA.Service
                     1
                 );
                 Debug.WriteLine(sw.ToString());
-                // adding negative value in qty is invalid
+                // successfully saved and removed product
                 Assert.IsTrue(sw.ToString().Contains("success"));
             }
         }
         [TestMethod]
-        public void AddingRemovingProductWithInvalidId()
+        public void TestAddingRemovingProductWithInvalidId()
         {
             using (StringWriter sw = new StringWriter())
             {
@@ -208,16 +210,17 @@ namespace InventoryMgmtQA.Service
                 );
                 Debug.WriteLine(sw.ToString());
                 // removing product, but id 2 does not exist
+                //the assertion is true. The test will pass if sw does contain 'Product not found, please try again'
                 Assert.IsTrue(sw.ToString().Contains("Product not found, please try again"));
 
-                //adding product
+                //successsfully add product
                 Assert.IsTrue(sw.ToString().Contains("success"));
             }
         }
 
         [TestMethod]
         //Work by Patrick Feniza
-        public void UpdateProduct()
+        public void TestUpdateProduct()
         {
             using (StringWriter sw = new StringWriter())
             {
@@ -235,7 +238,170 @@ namespace InventoryMgmtQA.Service
                 );
                 Debug.WriteLine(sw.ToString());
                 
+                //successfully update a product
                 Assert.IsTrue(sw.ToString().Contains("success"));
+            }
+        }
+
+
+        [TestMethod]
+        //Work by Patrick Feniza
+        public void TestGetListOfProducts()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                //if we not add product, no id will be generated.
+                Console.SetOut(sw);
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  3,
+                  6.23M
+              );
+
+                _inventoryManager.AddNewProduct(
+                 "TestProduct1",
+                 5,
+                 1.23M
+             );
+                // get id to remove the product
+                _inventoryManager.ListProducts(
+                  
+                );
+                Debug.WriteLine(sw.ToString());
+
+                //Successfully fetch products
+                Assert.IsTrue(sw.ToString().Contains("success"));
+            }
+        }
+
+        [TestMethod]
+        //Work by Patrick Feniza
+        public void TestGetListOfProductsWithUpdate()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                //if we not add product, no id will be generated.
+                Console.SetOut(sw);
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  3,
+                  6.23M
+              );
+
+                _inventoryManager.UpdateProduct(
+                 1,
+                 5
+             );
+                // get id to remove the product
+                _inventoryManager.ListProducts(
+
+                );
+                Debug.WriteLine(sw.ToString());
+
+                //Successfully add, update, and ready products
+                Assert.IsTrue(sw.ToString().Contains("success"));
+            }
+        }
+
+        [TestMethod]
+        //Work by Patrick Feniza
+        public void TestGetListOfProductsWithUpdateAndRemoveProduct()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                //if we not add product, no id will be generated.
+                Console.SetOut(sw);
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  3,
+                  6.23M
+              );
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  4,
+                  7.23M
+              );
+
+                _inventoryManager.UpdateProduct(
+                 1,
+                 5
+             );
+                _inventoryManager.RemoveProduct(
+                 2                
+             );
+                // get id to remove the product
+                _inventoryManager.ListProducts(
+
+                );
+                Debug.WriteLine(sw.ToString());
+
+                //Successfully add, update, delete, read a product
+                Assert.IsTrue(sw.ToString().Contains("success"));
+            }
+        }
+
+        [TestMethod]
+        //Work by Patrick Feniza
+        public void TestGetTotalValueAndListOfProducts()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                //if we not add product, no id will be generated.
+                Console.SetOut(sw);
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  3,
+                  6.23M
+              );
+                _inventoryManager.AddNewProduct(
+                  "TestProduct",
+                  4,
+                  7.23M
+              );
+
+                _inventoryManager.AddNewProduct(
+                 "TestProduct",
+                 4,
+                 20.23M
+             );
+                _inventoryManager.AddNewProduct(
+               "TestProduct",
+               8,
+               50.23M
+           );
+                // get id to remove the product
+                _inventoryManager.ListProducts(
+
+                );
+                _inventoryManager.GetTotalValue(
+
+                );
+                Debug.WriteLine(sw.ToString());
+
+                //Successfully fetch sum of all products price
+                Assert.IsTrue(sw.ToString().Contains("success"));
+            }
+        }
+
+        [TestMethod]
+        //Work by Patrick Feniza
+        public void TestFetchWithoutProducts()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                //if we not add product, no id will be generated.
+                Console.SetOut(sw);
+                
+                // get id to remove the product
+                _inventoryManager.ListProducts(
+
+                );
+              
+                Debug.WriteLine(sw.ToString());
+
+                //invalid, since there are no products added
+                //sw does not contain 'success' which pass the test
+                Assert.IsFalse(sw.ToString().Contains("success"));
             }
         }
     }
